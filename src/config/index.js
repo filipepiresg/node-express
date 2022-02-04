f31209470d3d4e6675f7a812a6899a2fb6934e67
Filipe Pires <liipepires@gmail.com>
2022-02-04 19:02:07 -0300

    feat: add sequelize

---
+++ src/config/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "development": {
+    "username": "docker",
+    "password": "docker",
+    "database": "database_dev",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "docker",
+    "password": "docker",
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "docker",
+    "password": "docker",
+    "database": "database_prd",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "staging": {
+    "username": "docker",
+    "password": "docker",
+    "database": "database_stg",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+}
